d9c45c6ea57c846ed6f2ea3b2b1235a29e20f284
Diab <emailcodydiab@gmail.com>
2020-03-30 09:11:52 -0500

    started JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("this is an alert! JS is running!");
+var playerName = window.prompt("What is you robot's name?");
+
+console.log(playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight()
